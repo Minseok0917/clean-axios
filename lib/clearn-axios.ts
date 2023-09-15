d1f451d98aa26a069e6170040c0027c723e93467
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2023-09-15 17:27:49 +0900

    feat(ing) : typescript migration2

--- lib/clearn-axios.ts
+++ lib/clearn-axios.ts
@@ -3,23 +3,24 @@ import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 interface config {
   [key: string]: (...args: any[]) => AxiosRequestConfig;
 }
+type msss = Record<string, (...args: any[]) => AxiosRequestConfig>;
 
 const instance = axios.create({
   baseURL: "https://naver.com",
 });
 
 const services = {
-  fetchUser: (name: string): AxiosRequestConfig => ({
+  fetchUser: (name: string) => ({
     method: "GET",
     url: `/${name}`,
   }),
-  createUser: (name: string): AxiosRequestConfig => ({
+  createUser: (name: string) => ({
     method: "POST",
     url: `/${name}`,
   }),
 };
 
-type FunctionType = (...args: any[]) => any;
+type FunctionType = (...args: any[]) => AxiosRequestConfig;
 
 function upgrade<T extends Record<string, FunctionType>>(services: T) {
   type FunctionKeys = keyof T;
@@ -33,7 +34,7 @@ function upgrade<T extends Record<string, FunctionType>>(services: T) {
 
   for (const key in services) {
     const fn = services[key];
-    upgraded[key] = (...args: any[]) => {
+    upgraded[key] = (...args: any[]): Promise<AxiosResponse<any, any>> => {
       console.log(`Calling ${key} with `, args);
       return fn(...args);
     };
@@ -43,3 +44,16 @@ function upgrade<T extends Record<string, FunctionType>>(services: T) {
 }
 
 const { fetchUser } = upgrade(services);
+
+const services2: msss = {
+  fetchUser: (name: string): AxiosRequestConfig => ({
+    method: "GET",
+    url: `/${name}`,
+  }),
+  createUser: (name: string): AxiosRequestConfig => ({
+    method: "POST",
+    url: `/${name}`,
+  }),
+};
+
+// function backup<T extends typeof services>(services: T) {}
